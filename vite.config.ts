90410d05c7e888ff4467fc2ca1e8bbc48b7140d7
Muhammad Irfan Jazuli <mirfanjazuli7@gmail.com>
2026-06-24 16:05:47 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         tailwindcss(),
         wayfinder({
             formVariants: true,
-            command: 'php8.5 artisan wayfinder:generate',
+            // command: 'php8.5 artisan wayfinder:generate',
         }),
     ],
 });
